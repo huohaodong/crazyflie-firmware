3ad674eaea88b1abf0051821836a6032fc55c878
Huo Haodong <huohaodong.io@gmail.com>
2024-01-29 14:01:52 +0800

    add raft files

---
+++ src/deck/drivers/interface/raft.h
@@ -0,0 +1,6 @@
+#ifndef __RAFT_H__
+#define __RAFT_H__
+
+#define RAFT_DEBUG_ENABLE
+
+#endif
---
+++ src/deck/drivers/src/raft.c
@@ -0,0 +1,6 @@
+#include "raft.h"
+
+#ifndef RAFT_DEBUG_ENABLE
+  #undef DEBUG_PRINT
+  #define DEBUG_PRINT
+#endif
