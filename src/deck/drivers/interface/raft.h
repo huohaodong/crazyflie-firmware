78ce30d21dbc6f7604d93820930446227cf8f17c
Huo Haodong <huohaodong.io@gmail.com>
2024-01-29 15:46:45 +0800

    add raft init func

--- src/deck/drivers/interface/raft.h
+++ src/deck/drivers/interface/raft.h
@@ -7,29 +7,40 @@
 
 #define RAFT_DEBUG_ENABLE
 
+/* Queue Constants */
+#define RAFT_RX_QUEUE_SIZE 5
+#define RAFT_RX_QUEUE_ITEM_SIZE sizeof(UWB_Data_Packet_t)
+
 /* Raft Constants */
 #define RAFT_LOG_SIZE_MAX 100
 #define RAFT_CLUSTER_PEER_NODE_COUNT_MAX 10
 #define RAFT_VOTE_FOR_NO_ONE UWB_DEST_EMPTY
 
 typedef enum {
-  FOLLOWER, CANDIDATE, LEADER
+  RAFT_STATE_FOLLOWER,
+  RAFT_STATE_CANDIDATE,
+  RAFT_STATE_LEADER
 } RAFT_STATE;
 
 typedef struct {
   uint16_t term;
   uint16_t index;
   char *command; // TODO: COMMAND enum type definition
+} Raft_Log_Item_t;
+
+typedef struct {
+  uint16_t size;
+  Raft_Log_Item_t items[RAFT_LOG_SIZE_MAX];
 } Raft_Log_t;
 
 typedef struct {
   SemaphoreHandle_t mu;
   UWB_Address_t peerNodes[RAFT_CLUSTER_PEER_NODE_COUNT_MAX]; /* peer nodes in current raft cluster configuration */
   uint8_t voteCount; /* granted vote count from peer nodes in current term */
-  RAFT_STATE currentState; /* latest term server has seen (initialized to 0 on first boot, increases monotonically) */
+  RAFT_STATE currentState;
   uint16_t currentTerm; /* latest term server has seen (initialized to 0 on first boot, increases monotonically) */
   UWB_Address_t voteFor; /* candidate that received vote in current term (or null if none), RAFT_VOTE_FOR_NO_ONE == null */
-  Raft_Log_t log[RAFT_LOG_SIZE_MAX]; /* log entries, each entry contains command for state machine, and term when entry was received by leader (first index is 1) */
+  Raft_Log_t log; /* log entries, each entry contains command for state machine, and term when entry was received by leader (first index is 1) */
   uint16_t commitIndex; /* index of highest log entry known to be committed (initialized to 0, increases monotonically) */
   uint16_t lastApplied; /* index of highest log entry known to be applied to state machine (initialized to 0, increases monotonically) */
   uint16_t nextIndex[RAFT_CLUSTER_PEER_NODE_COUNT_MAX]; /* for each server, index of the next log entry to send to that server (initialized to leader last log index + 1) */
@@ -56,7 +67,7 @@ typedef struct {
   UWB_Address_t leaderId; /* so follower can redirect clients */
   uint16_t prevLogIndex; /* index of log entry immediately preceding new ones */
   uint16_t prevLogTerm; /* term of prevLogIndex entry */
-  Raft_Log_t entries[RAFT_LOG_ENTRIES_SIZE_MAX]; /* log entries to store (empty for heartbeat; may send more than one for efficiency) */
+  Raft_Log_Item_t entries[RAFT_LOG_ENTRIES_SIZE_MAX]; /* log entries to store (empty for heartbeat; may send more than one for efficiency) */
   uint16_t leaderCommit; /* leader's commitIndex */
 } __attribute__((packed)) Raft_Append_Entries_Args_t;
 
@@ -65,7 +76,7 @@ typedef struct {
   bool success; /* true if follower contained entry matching prevLogIndex and prevLogTerm */
 } __attribute__((packed)) Raft_Append_Entries_Reply_t;
 
-void raftInit(Raft_Node_t *node);
+void raftInit();
 void raftSendRequestVote(UWB_Address_t address, Raft_Request_Vote_Args_t *args);
 void raftProcessRequestVoteReply(Raft_Request_Vote_Reply_t *reply);
 void raftSendAppendEntries(UWB_Address_t address, Raft_Append_Entries_Args_t *args);
--- src/deck/drivers/src/raft.c
+++ src/deck/drivers/src/raft.c
@@ -1,3 +1,4 @@
+#include "FreeRTOS.h"
 #include "raft.h"
 
 #ifndef RAFT_DEBUG_ENABLE
@@ -8,3 +9,33 @@
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 
+static QueueHandle_t rxQueue;
+static Raft_Node_t raftNode;
+static Raft_Log_Item_t EMPTY_LOG_ITEM = {
+    .term = 0,
+    .index = 0,
+    .command = "NULL"
+};
+
+void raftInit() {
+  xSemaphoreCreateMutex();
+  rxQueue = xQueueCreate(RAFT_RX_QUEUE_SIZE, RAFT_RX_QUEUE_ITEM_SIZE);
+  raftNode.mu = xSemaphoreCreateMutex();
+//  raftNode.peerNodes = ? TODO: init peer
+  raftNode.voteCount = 0;
+  raftNode.currentState = RAFT_STATE_FOLLOWER;
+  raftNode.currentTerm = 0;
+  raftNode.voteFor = RAFT_VOTE_FOR_NO_ONE;
+  raftNode.log.items[0] = EMPTY_LOG_ITEM;
+  raftNode.log.size = 1;
+  raftNode.commitIndex = 0;
+  raftNode.lastApplied = 0;
+  for (int i = 0; i < RAFT_CLUSTER_PEER_NODE_COUNT_MAX; i++) {
+    raftNode.nextIndex[i] = raftNode.log.items[raftNode.log.size - 1].index + 1;
+    raftNode.matchIndex[i] = 0;
+  }
+  raftNode.lastHeartbeatTime = xTaskGetTickCount();
+//  TODO: election timer
+//  TODO: heartbeat timer
+//  TODO: log applier timer
+}
