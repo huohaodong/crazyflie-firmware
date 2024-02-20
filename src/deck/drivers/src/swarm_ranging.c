a0da82a4a67e59ba5bec896bbd78524a3d6a1ed8
Huo Haodong <huohaodong.io@gmail.com>
2024-02-20 13:50:46 +0800

    bugfix

--- src/deck/drivers/src/swarm_ranging.c
+++ src/deck/drivers/src/swarm_ranging.c
@@ -519,6 +519,7 @@ void neighborSetAddRelation(Neighbor_Set_t *set, UWB_Address_t from, UWB_Address
   ASSERT(to <= NEIGHBOR_ADDRESS_MAX);
   if (!neighborBitSetHas(&set->twoHopReachSets[to], from)) {
     neighborBitSetAdd(&set->twoHopReachSets[to], from);
+    neighborSetHooksInvoke(&set->neighborTopologyChangeHooks, to);
   }
 }
 
@@ -527,6 +528,7 @@ void neighborSetRemoveRelation(Neighbor_Set_t *set, UWB_Address_t from, UWB_Addr
   ASSERT(to <= NEIGHBOR_ADDRESS_MAX);
   if (neighborBitSetHas(&set->twoHopReachSets[to], from)) {
     neighborBitSetRemove(&set->twoHopReachSets[to], from);
+    neighborSetHooksInvoke(&set->neighborTopologyChangeHooks, to);
   }
 }
 
