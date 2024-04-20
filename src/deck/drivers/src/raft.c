5ea22b292e349a6782d6413d697ef7973ea7363e
Huo Haodong <huohaodong.io@gmail.com>
2024-04-20 23:13:08 +0800

    bug fixed

--- src/deck/drivers/src/raft.c
+++ src/deck/drivers/src/raft.c
@@ -666,7 +666,7 @@ void raftSendRequestVote(UWB_Address_t peerAddress) {
   args->lastLogTerm = raftNode.log.items[raftNode.log.size - 1].term;
   args->stability = getCurrentStabilityMetric();
   DEBUG_PRINT("raftSendRequestVote: %u send request vote to %u.\n", raftNode.me, peerAddress);
-  uwbSendDataPacketBlock(&dataTxPacket);
+  uwbSendDataPacketBlock(&dataTxPacket)
 }
 
 void raftProcessRequestVote(UWB_Address_t peerAddress, Raft_Request_Vote_Args_t *args) {
@@ -694,7 +694,7 @@ void raftProcessRequestVote(UWB_Address_t peerAddress, Raft_Request_Vote_Args_t 
     raftSendRequestVoteReply(args->candidateId, raftNode.currentTerm, true);
     return;
   }
-  if (raftNode.voteFor != RAFT_VOTE_FOR_NO_ONE || raftNode.voteFor != args->candidateId) {
+  if (raftNode.voteFor != RAFT_VOTE_FOR_NO_ONE && raftNode.voteFor != args->candidateId) {
     DEBUG_PRINT("raftProcessRequestVote: I %u have already granted vote to %u this term, don't grant vote.\n",
                 raftNode.me,
                 raftNode.voteFor);
